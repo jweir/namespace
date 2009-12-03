0b93319092a210225d82e40d6209c965cc735c6f
J. Weir <john@smokingun.com>
2009-12-03 15:54:58 -0500

    namespace namespaces itself

--- namespace.js
+++ namespace.js
@@ -5,11 +5,11 @@
   // useage
   //  (function(){
   //    namespace("space", public_func);
-  //  
+  //
   //    function private_func(n){ return [n, "private"].join(" and ")};
   //    function public_func(n){ return private_func(n);}
   //  })();
-  //  
+  //
   //  space.public_func("good"); /* => "good and private" */
   //
   // allows one or more functions
@@ -17,16 +17,29 @@
   //
   // accepts nested namespaces, even if the parent does not yet exist
   //    namespaces("foo.bar", func)
+  //
+  // if no string is given as the first argument, the functions will be scoped to the root
 
-  root.namespace = function namespace(namespace, functions) {
+  namespace(namespace);
+
+  function namespace(namespace, functions) {
+    if(typeof arguments[0] != "string"){
+      return namespace_for_root.apply(this, arguments);
+    }
+
     var parts = arguments[0].split(".")
         space = namespaceFor(parts);
 
     for (var i=1; i < arguments.length; i++) {
       space[functionName(arguments[i])] = arguments[i]
     };
-
   };
+
+  function namespace_for_root(functions){
+    for (var i=0; i < arguments.length; i++) {
+      root[functionName(arguments[i])] = arguments[i]
+    };
+  }
 
   function functionName(fn) {
     return fn.name ? fn.name : fn.toString().match(/^\s*function\s+([^\s\(]+)/)[1];
